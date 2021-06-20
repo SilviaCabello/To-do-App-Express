2d3ff886c180153f079ae6a853be005a71304667
SilviaCabello <silviacabellom@gmail.com>
2021-06-20 20:25:58 +0200

    create components, set state and create input

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,3 +1,13 @@
-const Input = () => {};
+import { useState } from "react";
+
+const Input = () => {
+    const [toDo, setToDo] = useState("");
+
+    return (
+        <div>
+            <input type="text"/>
+        </div>
+    )
+};
 
 export default Input;
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,5 +1,9 @@
 const TaskLIst = () => {
-
-}
+  return (
+    <div>
+      <p>This is TaskLIst Component</p>
+    </div>
+  );
+};
 
 export default TaskLIst;
